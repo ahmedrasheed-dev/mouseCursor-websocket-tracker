84f4c92d58fd6096af83b5a2dcd05d95b4a73c97
ahmadrasheed <ahmadrasheedaaa@gmail.com>
2025-08-21 19:42:56 +0500

    practicing basics of websockets

--- server.js
+++ server.js
@@ -9,26 +9,35 @@ let clients = new Map();
 wss.on("connection", (ws) => {
   const id = randomUUID();
   console.log(`New client connected: ${id}`);
-  clients.set(id, ws);
+  clients.set(id, { ws, x: 0, y: 0 });
+
   ws.on("message", (message) => {
-    if (message.toString() === "ping") {
-      console.log("Got ping from", id);
-      for (const [clientId, client] of clients) {
-        if (clientId !== id) {
-          client.send(`Ping from ${id}`);
-        }
-      }
-      return;
-    }
-    for (const [clientId, client] of clients) {
-      if (clientId !== id) {
-        client.send(`User ${id} sendt message: ${message}`);
+    // if (message.toString().message === "ping") {
+    //   console.log("Got ping from", id);
+    //   for (const [clientId, client] of clients) {
+    //     if (clientId !== id) {
+    //       client.send(`Ping from ${id}`);
+    //     }
+    //   }
+    //   return;
+    // }
+    const parsed = JSON.parse(message);
+    // Ensure numbers
+    const x = Number(parsed.x);
+    const y = Number(parsed.y);
+    clients.set(id, { ...clients.get(id), x, y });
+
+    //this is broadcasting
+    for (const [clientId, clientData] of clients) {
+      const client = clientData.ws;
+      if (clientId !== id && client.readyState === 1) {
+        client.send(`User ${id} cords: ${x}, ${y}`);
       }
     }
   });
 
   ws.on("close", () => {
-    delete clients[ws.id];
+    delete clients.delete(id);
   });
 });
 const PORT = 3000;
